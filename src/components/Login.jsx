2319e1e2cee10b90ab6cf48dd3a437de7f194b90
Sai Naga Anu Teja Gunda <77635770+GUNDAANUTEJ@users.noreply.github.com>
2022-03-07 22:27:04 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
         } else {
             const email = Email
             const password = Pass
-            const result = await fetch("http://localhost:3300/login", {
+            const result = await fetch("/login", {
                 method: "POST",
                 headers: {
                     "content-type": "application/json"
