ac5bf668355da601c8c0e2c8011c8676df0a2850
Sai Naga Anu Teja Gunda <77635770+GUNDAANUTEJ@users.noreply.github.com>
2022-03-08 20:51:04 +0530

    Update Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,23 +1,7 @@
-import { useEffect } from "react"
-import axios from 'axios'
 import './dashboard.css'
 import DataFetching from './DataFetching'
-import {  useNavigate } from "react-router-dom"
 
 const Dashboard = () => {
-
-    const navigate = useNavigate();
-
-//     useEffect(() => {
-//         const callBack = async () => {
-//             await axios.get("https://study-buddy-bckend.herokuapp.com/dashboard")
-//                 .then((result) => {
-//                     if (!result.data)
-//                         navigate('/')
-//                 })
-//         }
-//         callBack();
-//     }, [navigate]);
 
     return (
         <>
