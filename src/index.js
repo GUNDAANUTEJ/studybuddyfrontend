e8c1a3fed568c760b7c6e975901a9661725a414d
Sai Naga Anu Teja Gunda <77635770+GUNDAANUTEJ@users.noreply.github.com>
2022-03-28 22:58:58 +0530

    Auth0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <>
-    <App />
+    <Auth0Provider
+      domain="studybuddyauth.us.auth0.com"
+      clientId="mwYt1WFScuj8zocxce1KsS2A2i2VGhG5"
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
   </>,
   document.getElementById("root")
 );
