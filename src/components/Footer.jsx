880d3e6f77401ebbb04e6fa8a0255d36c52a610d
GUNDAANUTEJ <77635770+GUNDAANUTEJ@users.noreply.github.com>
2022-04-04 22:33:45 -0500

    Updated proposal and RFP links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,8 @@ function Footer() {
                 <a href="https://github.com/GUNDAANUTEJ/StudyBuddy/projects/1">Project Board</a>
                 <a href="https://github.com/GUNDAANUTEJ/StudyBuddy/wiki">Wiki</a>
                 <a href="https://github.com/GUNDAANUTEJ/StudyBuddy/actions">Actions</a>
+                <a href="https://github.com/Maruthi158/691-01-F21-DevRFP-Group5/blob/main/Proposal.md">Proposal</a>
+                <a href="https://github.com/Maruthi158/691-01-F21-RFP-Group2/blob/main/rfp-study.md">RFP</a>
             </div>
         </footer>
     </>
